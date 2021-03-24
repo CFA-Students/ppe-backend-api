5d646e3724a27259445ed408d99ae738a57662a7
E-jarod <ejilane.jarod@gmail.com>
2021-03-24 17:40:26 +0100

    created travels models

---
+++ src/travels/travel.ts
@@ -0,0 +1,11 @@
+export class Travel {
+  readonly id: number;
+  readonly cratedAt: string;
+  readonly updatedAt: string;
+  readonly agency: string;
+  readonly destination: string;
+  readonly priceHt: number;
+  readonly price: number;
+  readonly description: string;
+  readonly image: string;
+}
---
+++ src/travels/travels.ts
@@ -0,0 +1,5 @@
+import { Travel } from './travel';
+
+export class Travels {
+  [key: number]: Travel;
+}
