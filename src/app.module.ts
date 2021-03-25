0f3f1087728da2673682868f45461f367d73fe85
E-jarod <ejilane.jarod@gmail.com>
2021-03-25 08:37:50 +0100

    added users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TravelsModule } from './travels/travels.module';
 import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TravelsModule, CategoriesModule],
+  imports: [TravelsModule, CategoriesModule, UsersModule],
   controllers: [],
   providers: [],
 })
