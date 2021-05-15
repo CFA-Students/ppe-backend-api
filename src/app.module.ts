1a5b32510462ee0c728bf9757d9f8a5f53b8c8fa
E-jarod <ejilane.jarod@gmail.com>
2021-05-15 11:47:03 +0200

    g auth module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TravelsModule } from './travels/travels.module';
 import { CategoriesModule } from './categories/categories.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { UsersModule } from './users/users.module';
     TravelsModule,
     CategoriesModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
