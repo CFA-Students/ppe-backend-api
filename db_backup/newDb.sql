d8173c00846137a5c302298e672ddd2306f699c5
E-jarod <ejilane.jarod@gmail.com>
2021-06-04 09:22:23 +0200

    relation ok

--- db_backup/newDb.sql
+++ db_backup/newDb.sql
@@ -6,77 +6,71 @@ SET NAMES utf8;
 CREATE TABLE `User`
 (
     `id`                INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`              VARCHAR(255) NOT NULL,
-    `email`             VARCHAR(255) NOT NULL,
-    `email_verified_at` TIMESTAMP    NULL     DEFAULT NULL,
+    `username`          VARCHAR(255) NOT NULL,
     `password`          VARCHAR(255) NOT NULL,
+    `name`              VARCHAR(100) NOT NULL,
+    `surname`           VARCHAR(100) NOT NULL,
+    `email`             VARCHAR(255) NOT NULL,
+    `tel`               VARCHAR(15)  NOT NULL,
     `created_at`        TIMESTAMP    NOT NULL,
+    `email_verified_at` TIMESTAMP    NULL     DEFAULT NULL,
     `updated_at`        TIMESTAMP    NULL     DEFAULT NULL,
     `is_online`         TINYINT(1)   NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
-
-INSERT INTO `User`
-VALUES (1, 'Jarod', 'ejilane.jarod@gmail.com', '2021-03-10 21:38:33', 'jarod321', '2021-03-10 21:38:33',
-        '2021-03-10 21:38:33', 0),
-       (2, 'Benjamin', 'benjamim@gmail.com', '2021-03-10 21:38:33', 'benjamin123', '2021-03-10 21:38:33',
-        '2021-03-10 21:38:33', 1),
-       (3, 'Test', 'test@gmail.com', '2021-03-10 21:38:33', 'test', '2021-03-10 21:38:33', '2021-03-10 21:38:33', 0);
+) ENGINE = InnoDB;
 
 CREATE TABLE `Supplier`
 (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
+    `id`      INT UNSIGNED PRIMARY KEY NOT NULL REFERENCES User (`id`),
+    `name`    VARCHAR(100)             NOT NULL,
+    `surname` VARCHAR(100)             NOT NULL
+) ENGINE = InnoDB;
 
 CREATE TABLE `Admin`
 (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
+    `id` INT UNSIGNED PRIMARY KEY NOT NULL REFERENCES User (`id`)
+) ENGINE = InnoDB;
 
 CREATE TABLE `Client`
 (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
-
-CREATE TABLE `Perform`
-(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
+    `id` INT UNSIGNED PRIMARY KEY NOT NULL REFERENCES User (`id`)
+) ENGINE = InnoDB;
 
 CREATE TABLE `Reservation`
 (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
+) ENGINE = InnoDB;
+
+CREATE TABLE `Perform`
+(
+    `id_client`      INT UNSIGNED NOT NULL,
+    `id_reservation` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id_client`, `id_reservation`),
+    FOREIGN KEY (`id_client`) REFERENCES Client (`id`),
+    FOREIGN KEY (`id_reservation`) REFERENCES Reservation (`id`)
+) ENGINE = InnoDB;
+
+# 1,N
+CREATE TABLE `Payment`
+(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+# 1,N
+CREATE TABLE `Execute`
+(
+    `id_reservation` INT UNSIGNED NOT NULL,
+    `id_payment`     INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id_reservation`, `id_payment`),
+    FOREIGN KEY (`id_reservation`) REFERENCES Reservation (`id`),
+    FOREIGN KEY (`id_payment`) REFERENCES Payment (`id`)
+) ENGINE = InnoDB;
 
 CREATE TABLE `Vehicle`
 (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
-
-CREATE TABLE `Execute`
-(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
-
-CREATE TABLE `Payment`
-(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4;
+) ENGINE = InnoDB;
