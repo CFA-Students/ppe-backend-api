1dbb0d73b0349a2626b8d158b835763ba42e64d9
E-jarod <ejilane.jarod@gmail.com>
2021-06-14 21:07:41 +0200

    vehicles GET works

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { Vehicle } from './vehicle.entity';
+import { VehiclesService } from './vehicles.service';
 
 @Controller('vehicles')
-export class VehiclesController {}
+export class VehiclesController {
+  constructor(private readonly vehiclesService: VehiclesService) {}
+
+  @Get()
+  async findAll(): Promise<Vehicle[]> {
+    const allVehicles = await this.vehiclesService.findAll();
+    if (allVehicles.length <= 0)
+      throw new HttpException(
+        'No vehicles found',
+        HttpStatus.NOT_FOUND
+      );
+    return allVehicles;
+  }
+
+  @Get(':id')
+  async findById(
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<Vehicle> {
+    const vehicle = await this.vehiclesService.findById(id);
+    if (!vehicle)
+      throw new HttpException(
+        'No vehicle found',
+        HttpStatus.NOT_FOUND
+      );
+    return vehicle;
+  }
+
+  @Post()
+  @HttpCode(201)
+  async insert(@Body('vehicle') vehicle: Vehicle): Promise<void> {
+    await this.vehiclesService.insert(vehicle);
+  }
+
+  @Put()
+  async update(@Body('vehicle') vehicle: Vehicle): Promise<void> {
+    await this.vehiclesService.update(vehicle);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    await this.vehiclesService.delete(id);
+  }
+}
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Vehicle } from './vehicle.entity';
 
 @Injectable()
-export class VehiclesService {}
+export class VehiclesService {
+  constructor(
+    @InjectRepository(Vehicle)
+    private vehiclesRepository: Repository<Vehicle>
+  ) {}
+
+  async findAll(): Promise<Vehicle[]> {
+    return await this.vehiclesRepository.find();
+  }
+
+  async findById(id: number): Promise<Vehicle> {
+    return await this.vehiclesRepository.findOne(id);
+  }
+
+  async insert(newVehicle: Vehicle): Promise<void> {
+    await this.vehiclesRepository.insert(newVehicle);
+  }
+
+  async update(updatedVehicle: Vehicle): Promise<void> {
+    await this.vehiclesRepository.save(updatedVehicle);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.vehiclesRepository.delete(id);
+  }
+}
